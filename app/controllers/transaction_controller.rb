78f980fa8ebfd168b8b2339f37b1d22ce6c6b256
Danny <barakadan421@gmail.com>
2023-01-09 20:10:19 +0300

    Transaction controller

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,2 +1,24 @@
 class TransactionController < ApplicationController
+  
+  def index
+    @transactions = Transaction.all
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user_id = current_user.id
+    if @transaction.save
+      redirect_to transactions_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount, :user_id)
+  end
 end
