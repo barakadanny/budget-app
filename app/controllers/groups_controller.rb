01f6c112ecfb2595a8ebba05bed158892e3cd604
Danny <barakadan421@gmail.com>
2023-01-09 13:12:42 +0300

    Groups controller index, show, create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,29 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      format.html {
+        redirect_to @group, notice: 'Group was succesfully created.'
+      }
+    else
+      format.html { render :new, status: :unprocessable_entitly }
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
