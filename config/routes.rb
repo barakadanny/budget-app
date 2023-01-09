5a731596102ef6f9e610207578126b8bfc1de76f
Danny <barakadan421@gmail.com>
2023-01-09 13:39:19 +0300

    Group route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :groups, only:[create]
   # Defines the root path route ("/")
   root "groups#index"
 end
