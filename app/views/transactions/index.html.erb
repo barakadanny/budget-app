5a3a784362fbc85ea57b4672aaeb5120695f942c
Danny <barakadan421@gmail.com>
2023-01-09 21:21:08 +0300

    Transaction routes

---
+++ app/views/transactions/index.html.erb
@@ -0,0 +1,1 @@
+<h1>Transaction Page</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :groups, only:[:new, :create, :show]
+  resources :transactions, only:[:new, :create, :show]
   # Defines the root path route ("/")
   get '/groups', to: 'groups#index'
+  get '/transactions', to: 'transactions#index'
   root "groups#index"
 end
