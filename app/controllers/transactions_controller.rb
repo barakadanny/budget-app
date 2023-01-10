666fdc2a0dd8733a5d1478bba14648dbfd2a5bea
Danny <barakadan421@gmail.com>
2023-01-10 23:29:58 +0300

    Fix group_id transaction :Bug ✊

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,8 +2,8 @@ class TransactionsController < ApplicationController
   access user: :all, admin: :all
   
   def index
-    @transactions = Transaction.all
     @group = Group.find(params[:group_id])
+    @transactions = Transaction.where(group_id: @group.id, user_id: current_user.id)
   end
 
   def show
@@ -18,7 +18,8 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new(transaction_params)
     @transaction.user_id = current_user.id
     if @transaction.save
-      redirect_to group_path(@transaction.group_id)
+      # redirect_to group_path(@transaction.group_id)
+      redirect_to transactions_path
     else
       render "new", status: :unprocessable_entity
     end
