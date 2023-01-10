aae8cc0aa3cf91a2ae986dfacb908f2fc8639869
Danny <barakadan421@gmail.com>
2023-01-10 14:05:51 +0300

    group foreign key in transaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_213329) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_110420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_213329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_transactions_on_group_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -46,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_213329) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "transactions", "groups"
   add_foreign_key "transactions", "users"
 end
