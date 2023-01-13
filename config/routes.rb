991eab96ee7e70308f39bf062a1f9a5aebf1b914
Danny <barakadan421@gmail.com>
2023-01-14 01:06:26 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'categories/index'
+  # get 'categories/new'
+  # get 'categories/create'
+  resources :categories, only:[:new, :create, :index]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :transactions, only: [:new, :create, :index]
   resources :groups, only:[:new, :create, :index] do
