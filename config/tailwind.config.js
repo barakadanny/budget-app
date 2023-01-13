f4d1d0e9c492f5232a10dd350faed4116012e944
Danny <barakadan421@gmail.com>
2023-01-13 14:42:30 +0300

    Solve tailwind config bug

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       colors: {
         bluenav: '#3778c2',
         secondColor: '#5fb523',
-        secondColorHover: rgba(95, 181, 35, 0.2),
+        secondColorHover: 'rgba(95, 181, 35, 0.2)',
       },
     },
   },
