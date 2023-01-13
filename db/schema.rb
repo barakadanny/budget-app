74676ad19064f9db25d99f08d6a746ef498fdb7d
Danny <barakadan421@gmail.com>
2023-01-14 01:05:48 +0300

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_122530) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_213641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -48,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_122530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "groups", "users"
   add_foreign_key "transactions", "groups"
   add_foreign_key "transactions", "users"
