83d3e84812457eaa32b9b66452596b1422912080
Danny <barakadan421@gmail.com>
2023-01-14 05:50:36 +0300

    Solve Linters att 1

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe 'Groups', type: :request do
     post user_session_path, params: { user: { email: user.email, password: user.password } }
     get root_path
   end
-  
-  describe 'GET index' do
 
+  describe 'GET index' do
     it 'should return http request' do
       expect(response).to have_http_status(:success)
     end
