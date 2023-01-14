15109a0fe58ffa5503911825fd0917d7a60f4274
Danny <barakadan421@gmail.com>
2023-01-14 04:22:58 +0300

    Validate category select

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :user_id, presence: true
+  validates_presence_of :category_ids
 
   belongs_to :user
   belongs_to :group
